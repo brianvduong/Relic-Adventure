837d49a01f10d8db08b8e06da9f4d6f19d79fd99
Brian Duong <brianvduong@gmail.com>
2020-05-08 15:23:36 -0700

    created a teleporter script in order to teleport to different parts of the map; walking around to each part of the map takes too long

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+
+    public Transform teleportTarget;
+    public CharacterController thePlayer;
+
+    void OnTriggerEnter(Collider other)
+    {
+        thePlayer.enabled = false;
+        thePlayer.transform.position = teleportTarget.transform.position;
+        thePlayer.enabled = true;
+    }
+
+}

3d27b7af30799f623861220d727e2b7f6e8166f2
Brian Duong <brianvduong@gmail.com>
2020-05-09 20:03:02 -0700

    created rotating item script, applied to relics, as well as some of the platforms

---
+++ Assets/Scripts/RotatingItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingItem : MonoBehaviour
+{
+    public float speed = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(Time.deltaTime*speed, 0, 0));
+    }
+}
